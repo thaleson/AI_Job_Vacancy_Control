79d23b20e345ab671a6653244b61e4a919f55d53
thaleson <thaleson19@hotmail.com>
2024-08-07 23:56:44 -0300

    Fix: add markdown in main page

--- main.py
+++ main.py
@@ -30,6 +30,31 @@ with st.sidebar:
         orientation='vertical'
     )
 
+      # Badges
+    st.markdown(
+        """
+        <div style="display: flex; justify-content: space-between;">
+            <div>
+                <a href="https://github.com/thaleson" target="_blank">
+                    <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" width="100" />
+                </a>
+            </div>
+            <div>
+                <a href="https://www.linkedin.com/in/thaleson-silva-9298a0296/" target="_blank">
+                    <img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" width="100" />
+                </a>
+            </div>
+            <div>
+                <a href="mailto:thaleson177@gmail.com" target="_blank">
+                    <img src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" width="80" />
+                </a>
+            </div>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+
 # Redirecionar para a página correspondente
 if option == 'Home':
     import pages.nav.home as home
